df32dd3df0a2c28d394e93e040a8ce247a42b065
ekeopre beredugo <ekeopreberedugo1@gmail.com>
2022-01-27 15:04:42 +0100

    working on nav responsivenes

--- js/menu.js
+++ js/menu.js
@@ -1,27 +1,36 @@
 const menu = document.getElementById("nav-menu");
 const firstMenuLine = document.getElementById("menu-line1");
 const secondMenuLine = document.getElementById("menu-line2");
-
 const navItems = document.getElementById("nav-menu-items");
 let clickCount = 'close';  // to set the state for the clicks to determine when to hide the navigation menu on click
 
 
 menu.addEventListener("click",  function toggleMenu() {
 
+  // open navigation menu on click
+
   if (clickCount  === 'close'){ 
     firstMenuLine.classList.add("slant-down");
         secondMenuLine.classList.add("slant-up");
         navItems.style.left = 0;
+        navItems.style.display = "block"
         clickCount  = 'open'
        }
-
+ // close navigation menu on click
         else if (clickCount  === 'open'){ 
          firstMenuLine.classList.remove("slant-down");
           secondMenuLine.classList.remove("slant-up");
-          navItems.style.left ="-20%"
+          navItems.style.display = "none"
           clickCount  = 'close'
+          
             }
+
+         
 })
+
+
+
+
 
 
 
