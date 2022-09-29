763b23edb6c5d5de0033da75d0470cabc0233ac7
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2022-09-29 22:35:01 +0100

    added form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
-
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["index.html" , "./src/input.css"],
+
+content: ["index.html" , "./src/input.css"],
   theme: {
     extend: {
-
       colors: {
         'body': "#0a0a0a"
     
       }
-
     },
 
 
-  screens: {
-    'xs': '320px',
-    ...defaultTheme.screens,
-  },
+    screens: {
+      'xs': '320px',
+      ...defaultTheme.screens,
+    },
+    
 
- 
-},
-  variants: {},
+
+
+
+  },
   plugins: [],
 }
-
