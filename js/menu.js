8d2e9652b1fd297b2f2ac68e86169b6706cf4404
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2022-10-30 19:50:59 +0100

    updated menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,58 +1,42 @@
 const menu = document.getElementById("hamburger-menu");
-const  navItems = document.getElementById("nav-menu-items-mobile");
-const body = document.getElementById("body")
-let clickCount = 'close'; 
-navItems.style.display = "none"
- // to set the state for the clicks to determine when to hide the navigation menu on click
+const navItems = document.getElementById("nav-menu-items-mobile");
+const body = document.getElementById("body");
+let clickCount = "close";
+navItems.style.display = "none";
+// to set the state for the clicks to determine when to hide the navigation menu on click
 
-
-menu.addEventListener("click",  function toggleMenu() {
-
+menu.addEventListener("click", function toggleMenu() {
   // open navigation menu on click
+  if (clickCount === "close") {
+    navItems.style.left = "0";
+    navItems.style.display = "block";
+    clickCount = "open";
+    console.log("menu open");
+  }
+  // close navigation menu on click
+  else if (clickCount === "open") {
+    navItems.style.display = "none";
+    clickCount = "close";
+  }
+});
 
-  if (clickCount  === 'close'){ 
-
-        navItems.style.left = '0';
-        navItems.style.display = "block"
-        clickCount  = 'open'
-        console.log("menu open")
-       }
- // close navigation menu on click
-        else if (clickCount  === 'open'){ 
-          navItems.style.display = "-20"
-          clickCount  = 'close'
-          
-            }
-
-        
-})
-
-
-window.addEventListener("mouseup", function(event){
-
-  if(event.target != navItems) {
-
+//navbar hides if anywhere else apart from the navbar is tapped/clicked
+window.addEventListener("mouseup", function (event) {
+  if (event.target != navItems) {
     navItems.style.display = "none";
   }
-
-})
-
+});
 
 // Enable hidden nav bar
+const nav = document.querySelector(".nav-container");
+let lastScrollY = window.scrollY;
 
-  const nav = document.querySelector(".nav-container");
-  let lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav--hidden");
+  } else {
+    nav.classList.remove("nav--hidden");
+  }
 
-  window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-      nav.classList.add("nav--hidden");
-    } else {
-      nav.classList.remove("nav--hidden");
-    }
-
-    lastScrollY = window.scrollY;
-  });
-
-
-  
-
+  lastScrollY = window.scrollY;
+});
