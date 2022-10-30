e337e31f590105293e1eb5d9d7d6301281e62abe
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2022-10-30 21:39:52 +0100

    updated dependecies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   mode: 'jit',
-purge: ["index.html" , "./src/input.css"],
+content: ["index.html" , "./src/input.css"],
   theme: {
     extend: {
       colors: {
